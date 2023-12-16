7f3a49540260edc2e96794bd8027de46b4f60fe2
Santosh9009 <patisantosh00@gmail.com>
2023-12-16 20:50:09 +0530

    Added authentication

--- Models/User.js
+++ Models/User.js
@@ -21,4 +21,7 @@ const UserSchema= new Schema({
   },
 });
 
-module.exports=mongoose.model('user',UserSchema);
+const user=mongoose.model('user',UserSchema);
+user.createIndexes();
+
+module.exports=user;
--- Routes/auth.js
+++ Routes/auth.js
@@ -4,18 +4,42 @@ const User = require("../Models/User");
 const { body, validationResult } = require("express-validator");
 
 router.post(
-  "/",
+  "/createuser",
   [
-    body("name").isLength({ min: 3 }),
-    body("email").isEmail(),
-    body("password").isLength({ min: 5 }),
+    body("name", "Enter a valid name").isLength({ min: 3 }),
+    body("email", "Enter a valid email").isEmail(),
+    body("password", "Passoword must have atleast 5 characters").isLength({
+      min: 5,
+    }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
+
     if (!errors.isEmpty()) {
-      return res.status(400).json({errors : errors.array()});
+      return res.status(400).json({ errors: errors.array() });
     }
-    res.send(req.body);
+    //checks weather if a user with same email exists already
+    try {
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ error: "A user with this email exists already" });
+      }
+      user = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+      })
+        .then((user) => res.json(user))
+        .catch((err) => {
+          console.log(err);
+          res.status(500).send("Internal Server Error");
+        });
+    } catch (error) {
+      console.log(error.message);
+      req.status(500).json("some error occured");
+    }
   }
 );
 
