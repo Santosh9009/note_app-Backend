d5947026fc057ea11835509451855da15817c30f
Santosh9009 <patisantosh00@gmail.com>
2023-12-24 21:03:51 +0530

    added express cors

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 const connectToMongo=require('./db');
 const express = require('express');
+var cors = require('cors')
 
 connectToMongo();
-
 const app = express();
 const port = 5000;
 
+app.use(cors());
 app.use(express.json());
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 
 app.use('/api/auth',require('./Routes/auth'));
 app.use('/api/notes',require('./Routes/notes'));
